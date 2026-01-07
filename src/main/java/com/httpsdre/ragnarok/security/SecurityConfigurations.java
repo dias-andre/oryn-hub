609e41084294bd0704b2f7a81934082851b60eda
https-dre <diaso.andre@outlook.com>
2026-01-07 20:32:50 -0300

    fix: add 'doFilter' in function

--- src/main/java/com/httpsdre/ragnarok/security/SecurityConfigurations.java
+++ src/main/java/com/httpsdre/ragnarok/security/SecurityConfigurations.java
@@ -1,23 +1,27 @@
 package com.httpsdre.ragnarok.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfigurations {
 
   private final SecurityFilter securityFilter;
 
-  public SecurityConfigurations(SecurityFilter securityFilter) {
-    this.securityFilter = securityFilter;
-  }
-
+  @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
             .csrf(csrf -> csrf.disable())
@@ -25,11 +29,24 @@ public class SecurityConfigurations {
                     session.sessionCreationPolicy((SessionCreationPolicy.STATELESS))
             )
             .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(HttpMethod.POST, "/users/discord/login").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/users/discord/auth").permitAll()
                     .requestMatchers(HttpMethod.POST, "/users/discord/register").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/docs").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
                     .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
   }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 }
