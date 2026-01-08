5e7de7ed5dbd26e2fefdf5035bf2f570c05f5caa
https-dre <diaso.andre@outlook.com>
2026-01-08 18:04:37 -0300

    feat: implement security service to control authentication in closed routes

---
+++ src/main/java/com/httpsdre/ragnarok/security/SecurityService.java
@@ -0,0 +1,25 @@
+package com.httpsdre.ragnarok.security;
+
+import com.httpsdre.ragnarok.models.MemberId;
+import com.httpsdre.ragnarok.repositories.MemberRepository;
+import com.httpsdre.ragnarok.types.SquadRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component("securityService")
+@RequiredArgsConstructor
+public class SecurityService {
+  private final MemberRepository memberRepository;
+
+  public boolean isMemberOf(UUID squadId, UUID userId) {
+    return memberRepository.existsById(new MemberId(userId, squadId));
+  }
+
+  public boolean isOwnerOf(UUID squadId, UUID userId) {
+    return memberRepository.findById(new MemberId(userId, squadId))
+            .map(m -> m.getRole() == SquadRole.OWNER)
+            .orElse(false);
+  }
+}
