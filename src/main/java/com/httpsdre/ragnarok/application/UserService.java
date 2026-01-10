cfc7b87083d82079fa697786c7134600f0e5dbe9
https-dre <diaso.andre@outlook.com>
2026-01-09 23:58:27 -0300

    feat: add route to fetch user squads

--- src/main/java/com/httpsdre/ragnarok/application/UserService.java
+++ src/main/java/com/httpsdre/ragnarok/application/UserService.java
@@ -1,20 +1,24 @@
 package com.httpsdre.ragnarok.application;
 
-import com.fasterxml.uuid.Generators;
 import com.httpsdre.ragnarok.dtos.GetCurrentUserRequest;
+import com.httpsdre.ragnarok.dtos.squad.SquadSummaryDTO;
 import com.httpsdre.ragnarok.dtos.user.LoginResponse;
 import com.httpsdre.ragnarok.dtos.user.UserSummaryDTO;
 import com.httpsdre.ragnarok.exceptions.NotFoundException;
 import com.httpsdre.ragnarok.exceptions.UnauthorizedException;
+import com.httpsdre.ragnarok.mappers.SquadMapper;
 import com.httpsdre.ragnarok.mappers.UserMapper;
+import com.httpsdre.ragnarok.models.Squad;
 import com.httpsdre.ragnarok.models.User;
 import com.httpsdre.ragnarok.providers.DiscordProvider;
+import com.httpsdre.ragnarok.repositories.SquadRepository;
 import com.httpsdre.ragnarok.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -22,6 +26,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserService {
   private final UserRepository userRepository;
+  private final SquadRepository squadRepository;
   private final DiscordProvider discord;
   private final TokenService tokenService;
 
@@ -56,19 +61,16 @@ public class UserService {
     return new LoginResponse(jwt, UserMapper.toSummary(user));
   }
 
-  public UserSummaryDTO getUserById(UUID id) {
-    User user = this.userRepository.findById(id)
-            .orElseThrow(() -> new NotFoundException("User not found"));
-    return UserMapper.toSummary(user);
-  }
-
-  public UserSummaryDTO toSummary(User user) {
-    return UserMapper.toSummary(user);
-  }
-
   public UserSummaryDTO getUser(UUID userId) {
     User user = this.userRepository.findById(userId)
             .orElseThrow(() -> new NotFoundException("User not found!"));
     return UserMapper.toSummary(user);
+  }
+
+  public List<SquadSummaryDTO> getUserSquads(UUID userId) {
+    List<Squad> squads = this.squadRepository.getUserSquads(userId);
+    return squads.stream()
+            .map(SquadMapper::toSummary)
+            .toList();
   }
 }
--- src/main/java/com/httpsdre/ragnarok/controllers/UserController.java
+++ src/main/java/com/httpsdre/ragnarok/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.httpsdre.ragnarok.controllers;
 
 import com.httpsdre.ragnarok.application.UserService;
+import com.httpsdre.ragnarok.dtos.squad.SquadSummaryDTO;
 import com.httpsdre.ragnarok.dtos.user.GetUserTokenRequest;
 import com.httpsdre.ragnarok.dtos.user.LoginResponse;
 import com.httpsdre.ragnarok.dtos.user.UserSummaryDTO;
@@ -11,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -30,5 +32,11 @@ public class UserController {
   @PreAuthorize("isAuthenticated()")
   public ResponseEntity<UserSummaryDTO> me(@AuthenticationPrincipal UUID userId) {
     return ResponseEntity.ok(this.userService.getUser(userId));
+  }
+
+  @GetMapping("/@me/squads")
+  @PreAuthorize("isAuthenticated()")
+  public ResponseEntity<List<SquadSummaryDTO>> getUserSquads(@AuthenticationPrincipal UUID userId) {
+    return ResponseEntity.ok(this.userService.getUserSquads(userId));
   }
 }
