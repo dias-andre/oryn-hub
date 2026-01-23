7c8ed6dff19e22f0c145b3c105f96f6d56523d65
https-dre <diaso.andre@outlook.com>
2026-01-23 01:51:08 -0300

    cleanup: remove unused imports

--- src/main/java/com/diasandre/oryn/controllers/UserController.java
+++ src/main/java/com/diasandre/oryn/controllers/UserController.java
@@ -8,8 +8,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
